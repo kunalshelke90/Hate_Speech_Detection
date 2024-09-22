20d138750337258cdfea355e45b43fa393071cf5
kunalshelke90 <shelkekunal90@gmail.com>
2024-09-23 00:35:46 +0530

    Done for deployment

---
+++ app.py
@@ -0,0 +1,55 @@
+from hate_speech.pipeline.train_pipeline import TrainPipeline
+from fastapi import FastAPI
+import uvicorn
+import sys
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from hate_speech.pipeline.prediction_pipeline import PredictionPipeline
+from hate_speech.exception import CustomException
+from hate_speech.constants import *
+
+
+text:str = "What is machine learing?"
+
+app = FastAPI()
+
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+
+
+
+@app.get("/train")
+async def training():
+    try:
+        train_pipeline = TrainPipeline()
+
+        train_pipeline.run_pipeline()
+
+        return Response("Training successful !!")
+
+    except Exception as e:
+        return Response(f"Error Occurred! {e}")
+    
+
+
+@app.post("/predict")
+async def predict_route(text):
+    try:
+
+        obj = PredictionPipeline()
+        text = obj.run_pipeline(text)
+        return text
+    except Exception as e:
+        raise CustomException(e, sys) from e
+    
+
+
+
+if __name__=="__main__":
+    uvicorn.run(app, host=APP_HOST, port=APP_PORT)
+
+
+    
